fd87d3c3708d9f30dc2e8fb889ec4ce993d06e6b
Phan Nhat Hoang <johnphan1608@gmail.com>
2023-03-19 19:38:39 +0800

    Added KMP.cpp to String/classicalPowerOf10Constant.cpp

--- String/classicalQuestionsLeadToTechniques/computePowerOf10ConstantFast.cpp
+++ String/classicalQuestionsLeadToTechniques/computePowerOf10ConstantFast.cpp
@@ -34,57 +34,38 @@ const int maxn = 1e5 + 5;
 const ll MOD = 1e9 + 7; // 998244353
 const ll INF = 1e9;
 
-vi solve1 (int n, int m, string s, string t) {
-    //Sol 1:  naive. for beginning position in s, check if t is match, by traversing through string t.
-    // Time: O((n-m+1)*m);
-    // Worst case: if m = n/2 -> O(n^2)
-    vi ans;
-    for (int i = 0; i+m-1 < n; i++) {
-        for (int j = 0; j < m; j++){
-            if (s[i+j] == t[j]) {
-                if (j == m-1) {
-                    ans.push_back(i);
-                }
-                continue;
-            } else {
-                break;
-            }
+ll mod_exponentiation(int a, int b, int n) {
+    int c = 0;
+    ll d = 1;
+    // let c is traversing from msb -> lsb, in the end: c should equal to b
+    // b_k, b_(k-1), ... , b_0 be binary representation of b
+    for (int i = 31; i >= 0; i--){
+        // seudo assume that 1 0 0 0 0
+        c *= 2; // ^2 everytime
+        d = (d*d) % n; // ^2 everytime
+        if ((b >> i) & 1) {
+            // if there is 1 one along the way traversing from msb to lsb
+            // add that to c: 1 0 0 0 1
+            // and multiply a, since a^c = a^(2^t + 1) = d * a^1
+            c = c+1;
+            d = (d*a) % n;
         }
     }
-    return ans;
-}
-
-vi solve2 (int n, int m, string s, string t) {
-    // Sol 2: Rabin-Karp
-    // time compelxity: O(m) preprocessing time + O((n-m+1)*m) worst case
-    // on average: it's better
-    // idea: hash t -> value v
-    // for each position start in s -> hash compute s[start:start+m] -> value v'
-    // if v == v'; -> ok
-    // however we can compute hash(s[start+1:start+m+1]) (hash_s+1) from s[start:start+m] (hash_s) in just O(1)
-    // hash_s+1 = 26*(hash_s - s[start] * 26^(m-1)) + s[start+m+1]
-    
-
+    return d;
 }
 
 void solve(){
-    // Task: Given two strings s and t. Find the matched position (beginning of t in s)
-    string s, t;
-    cin >> s >> t;
-    int n = s.size();
-    int m = t.size();
-    vi ans = solve1(n, m, s, t);
-    for (int i : ans) {
-        cout << i << " ";
-    }
-    cout << endl;
+    // Question: how to compute the constant 10^(m-1) fast in only O(lg m)
+    // A frequently occurring operation in number-theoretic computations is raising one number 
+    // to a power modulo another number, also known as modular exponentia- tion. More precisely, 
+    // we would like an efficient way to compute ab mod n, where a and b are nonnegative integers and n is a positive integer.
+    cout << mod_exponentiation(2, 4, 7) << endl;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int c;
-    cin >> c;
+    int c = 1;
     while(c--){
         solve();
     }
